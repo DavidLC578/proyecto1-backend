8e9ffe2727aed1b370375d49a71e86c9e8881e67
DavidLC578 <davidlatt2002@gmail.com>
2024-07-22 22:58:50 +0200

    feat: token de sesion

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
-import express from 'express';
-import connectDB from './config/database.js';
-import './config/dotenv.js';
-import userRoutes from './routes/userRoutes.js';
-import authRoutes  from './routes/authRoutes.js';
-import cors from 'cors'
+import express from "express";
+import connectDB from "./config/database.js";
+import "./config/dotenv.js";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 connectDB();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    // allowedHeaders: ['Content-Type', 'Authorization']
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  // allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
 
-
-app.use('/api', userRoutes);
-app.use('/api', authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", authRoutes);
 
 export default app;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,9 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const loginUser = async (req, res) => {
   const { name, password } = req.body;
@@ -22,8 +26,16 @@ export const loginUser = async (req, res) => {
         .json({ message: "Usuario o contraseña incorrectos" });
     }
 
+    // Generate token
+    console.log("JWT_SECRET:", process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
     // If authentication is successful
-    res.status(200).json({ message: "Usuario autenticado correctamente" });
+    res
+      .status(200)
+      .json({ message: "Usuario autenticado correctamente", token });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }

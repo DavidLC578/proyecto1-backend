ea8f32ab6fa41a6ae0d002b02b339ce6aff9ae55
DavidLC578 <davidlatt2002@gmail.com>
2024-09-02 15:44:18 +0200

    delete getUsers

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,6 @@ import { getUsers, createUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get('/', getUsers);
 router.post('/users/createUser', createUser);
 
 export default router;
